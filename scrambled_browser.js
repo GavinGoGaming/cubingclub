e811889521d440d108576379d352e2d7a56d475a
Gavin Fox <gavingogaming@gmail.com>
2024-11-13 15:34:14 -0800

    Update scrambled_browser.js

--- scrambled_browser.js
+++ scrambled_browser.js
@@ -1,11 +1,18 @@
-// made for cubingclub site, free to use. genScrable# returns string[], scramble# returns string
-const moves = ["U", "F", "L", "B", "R", "D"];
+const threebythree = ["U", "F", "L", "B", "R", "D"];
+const twobytwo = ["U", "R", "F"];
+function getMoves(size) {
+    if(size == 2) {
+        return twobytwo;
+    }else {
+        return threebythree;
+    }
+}
 function genScramble(amount, size) {
     let shkr = [];
     var lastMove = "";
     for (var x = 0; x < amount; x++) {
         function genMove() {
-            var move = moves[Math.floor(moves.length * Math.random())];
+            var move = getMoves(size)[Math.floor(getMoves(size).length * Math.random())];
             if(move == lastMove) return genMove();
             lastMove = move;
             var possibles = ["2", "'", ""];
